3042c7b27fbfffb52734755628b00e4b882f27ab
Zachary Durr <zdurr22@gmail.com>
2020-11-10 22:43:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,7 +24,7 @@ class Artist
     Song.all
   end
 
-  def find_or_create_by_name(artist_name)
+  def self.find_or_create_by_name(artist_name)
     found_artist = self.all.find {|artist| artist.name == artist_name}
     if found_artist
       found_artist
