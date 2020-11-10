fc4c1c7f5257bfced6d2dbabe331617a6233492b
Zachary Durr <zdurr22@gmail.com>
2020-11-10 22:59:17 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,11 @@ class MP3Importer
   end
 
   def files
+    @files ||= Dir.entries(@path).select |song| !File.directory?(song) && song.end_with?(".mp3")
+  end
 
+  def import
+    files.each {|song| Song.new_by_filename(song)}
   end
 
 end
